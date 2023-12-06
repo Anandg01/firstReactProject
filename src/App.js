89bea9a88bd34a47c8365d0a35bed8dcbc18a06c
Anandg01 <ptanandg@gmail.com>
2023-12-06 21:21:33 +0530

    Lets start React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expeseitem from "./Components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expeseitem></Expeseitem>
+    </div>
+  );
+}
+
+export default App;
